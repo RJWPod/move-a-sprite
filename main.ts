bb8d76809eb4a5315748d024879f8bf6f5543746
RJWPod <101339801+RJWPod@users.noreply.github.com>
2022-03-28 09:41:04 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,21 @@
+input.onButtonPressed(Button.A, function () {
+    Sprite.move(-1)
+})
+input.onGesture(Gesture.LogoUp, function () {
+    Sprite.change(LedSpriteProperty.Y, 1)
+})
+input.onButtonPressed(Button.AB, function () {
+    Sprite.set(LedSpriteProperty.X, 2)
+    Sprite.set(LedSpriteProperty.Y, 2)
+})
+input.onButtonPressed(Button.B, function () {
+    Sprite.move(1)
+})
+input.onGesture(Gesture.LogoDown, function () {
+    Sprite.change(LedSpriteProperty.Y, -1)
+})
+let Sprite: game.LedSprite = null
+Sprite = game.createSprite(2, 2)
+basic.forever(function () {
+	
+})
